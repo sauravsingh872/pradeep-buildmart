a006aed2527f432afaced39b69fa066c1dec1ab7
saurav kumar <sauravkumar8887252285@gmail.com>
2026-05-12 00:05:09 +0530

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -17,13 +17,14 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/PRADEEP_Traders/',
+
   plugins: [
     figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/app'),
